3ac63bb62779b7577d4a9f2c48feb2a0bf916736
ajiho <lujiahao@88.com>
2026-01-11 23:17:06 +0800

    parseHTML补全类型
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/util/parseHTML.ts
+++ src/util/parseHTML.ts
@@ -1,39 +1,37 @@
 import { isNull, isString, isUndefined } from 'is-what'
 
-export const parseHTML = (data, context, keepScripts = false) => {
-  let nodes = []
+export const parseHTML = (
+  data: string,
+  context?: Node | Document | null,
+  keepScripts: boolean = false,
+): Node[] => {
+  let nodes: Node[] = []
 
   if (!isString(data)) return nodes
 
-  //  如果 context 未指定，则创建一个新的 Document
   if (isNull(context) || isUndefined(context)) {
-    // 使用 DOMParser 将字符串解析成一个完整的文档
     const parser = new DOMParser()
     const doc = parser.parseFromString(data, 'text/html')
-
-    // 从文档中获取所有的子节点
     nodes = Array.from(doc.body.childNodes)
   } else {
-    if (context instanceof Node) {
-      context =
-        context.nodeType === Node.DOCUMENT_NODE
-          ? context
-          : context.ownerDocument
+    // 确保 context 是 Document
+    let doc: Document
+    if (context instanceof Document) {
+      doc = context
+    } else if (context instanceof Node && context.ownerDocument) {
+      doc = context.ownerDocument
     } else {
-      context = document
+      doc = document // fallback 到全局 document
     }
 
     // 创建临时容器
-    const range = context.createRange()
+    const range = doc.createRange()
     const frag = range.createContextualFragment(data)
     nodes = Array.from(frag.childNodes)
   }
 
-  // 是否保留 <script> 标签
   if (!keepScripts) {
-    nodes = nodes.filter(node => {
-      return !(node.nodeName && node.nodeName.toLowerCase() === 'script')
-    })
+    nodes = nodes.filter(node => node.nodeName.toLowerCase() !== 'script')
   }
 
   return nodes
