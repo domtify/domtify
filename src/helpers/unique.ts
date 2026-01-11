e3f57a066f789adb410ef0a792fd7fff5d788d1b
ajiho <lujiahao@88.com>
2026-01-11 23:21:01 +0800

    补全unique类型
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/helpers/unique.ts
+++ src/helpers/unique.ts
@@ -1,1 +1,6 @@
-export const unique = array => Array.from(new Set(array))
+/**
+ * 返回数组去重后的新数组
+ * @param array 要去重的数组
+ * @returns 去重后的数组
+ */
+export const unique = <T>(array: T[]): T[] => Array.from(new Set(array))
