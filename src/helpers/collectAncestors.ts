48d99838180bb8d40ec76669bdd571d6249e5bb7
ajiho <lujiahao@88.com>
2026-01-12 17:27:18 +0800

    collectAncestors方法类型补全
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/helpers/collectAncestors.ts
+++ src/helpers/collectAncestors.ts
@@ -1,11 +1,23 @@
 import { isUndefined } from 'is-what'
 import { unique } from './unique'
 
+interface CollectAncestorsOptions {
+  until?: HTMLElement[] // 遇到这些元素就停止
+  filter?: string // CSS 选择器，用于筛选
+  selector?: string // CSS 选择器，用于筛选
+}
+
+/**
+ * 收集元素的所有父元素，可选停止条件和筛选器
+ * @param elements 要收集父元素的元素数组
+ * @param options 可选参数：until、filter、selector
+ * @returns 去重后的父元素数组
+ */
 export const collectAncestors = (
-  elements,
-  { until = [], filter, selector } = {},
-) => {
-  let result = []
+  elements: HTMLElement[],
+  { until = [], filter, selector }: CollectAncestorsOptions = {},
+): HTMLElement[] => {
+  let result: HTMLElement[] = []
 
   for (const el of elements) {
     let parent = el.parentElement
